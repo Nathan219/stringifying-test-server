099618b615b9fc1b5e1c01354b764ab515d581f0
Nathan219 <Nathan219@gmail.com>
2016-05-04 13:52:23 -0700

    so many changes

--- routes/index.js
+++ routes/index.js
@@ -1,64 +1,71 @@
+'use strict'
+
 var express = require('express');
 var utils = require('../util.js');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/hard', function(req, res, next) {
-  var factor = 1;
-  var depth = 0;
-  var attempts = 2;
-  var goalTime = 1000;
-  var times = {};
-  do {
-    factor++;
-    depth = 0;
-    for(; depth < 4; depth) {
-      var newObject = utils.createObject(factor, depth);
-      times = utils.runTests(newObject, attempts);
-    }
-  } while (times.jsonedTime < goalTime && times.jsonedTime < goalTime);
-  var message = utils.writeIt(factor, depth, times);
-  res.render('index', utils.extractTimes({
-    title: 'Express',
-    message: message
-  }, times));
-});
+//router.get('/hard', function(req, res) {
+//  var factor = 1;
+//  var depth = 0;
+//  var attempts = 2;
+//  var goalTime = 1000;
+//  var times = {};
+//  do {
+//    factor++;
+//    depth = 0;
+//    for(; depth < 4; depth) {
+//      var newObject = utils.createObject(factor, depth);
+//      times = utils.runTests(newObject, attempts);
+//    }
+//  } while (times.jsonedTime < goalTime && times.jsonedTime < goalTime);
+//  var message = utils.writeOutFactorAndDepth(factor, depth);
+//  res.render('index', utils.extractTimes({
+//    title: 'Express',
+//    message: message
+//  }, times));
+//});
 
-router.get('/diy', function(req, res, next) {
+router.get('/diy', function(req, res) {
   var factor = parseInt(req.query.factor) || 1;
   var depth =  parseInt(req.query.depth) || 2;
+  var randomProperties = !!req.query.random;
+
+  depth = utils.makeBetweenMaxAndMinDepth(depth);
+  factor = utils.makeBetweenMaxAndMin(factor);
+  if (factor > 2) {
+    randomProperties = true;
+  }
   var attempts = 10;
-  var newObject = utils.createObject(factor, depth);
+  var newObject = utils.createObject(factor, depth, randomProperties);
   var times = utils.runTests(newObject, attempts);
-  var message = utils.writeIt(factor, depth, times);
+  var message = utils.writeOutFactorAndDepth(factor, depth, randomProperties);
   res.render('index', utils.extractTimes({
     title: 'Express',
     message: message
   }, times));
 });
 
-router.get('/', function(req, res, next) {
-  var factor = 1;
+router.get('/', function(req, res) {
+  var factor = 2;
   var depth = 2;
   var attempts = 10;
-  var goalTime = 1000;
-  var times = {};
-  var newObject = utils.createObject(factor, depth);
-  times = utils.runTests(newObject, attempts);
-  var message = utils.writeIt(factor, depth, times);
+  var newObject = utils.createObject(factor, depth, true);
+  var times = utils.runTests(newObject, attempts);
+  var message = utils.writeOutFactorAndDepth(factor, depth, true);
   res.render('index', utils.extractTimes({
     title: 'Express',
     message: message
   }, times));
 });
 
-router.get('/req', function(req, res, next) {
+router.get('/req', function(req, res) {
   var attempts = 50;
   console.log('');
-  var times = utils.runTests(req, attempts);
+  var times = utils.runTests(req, attempts, true);
   res.render('index', utils.extractTimes({
     title: 'Express',
-    message: message
+    message: 'The REQ object containing ' + Object.keys(req).length + ' and is Circular'
   }, times));
 });
 
--- util.js
+++ util.js
@@ -2,100 +2,101 @@
 var randomstring = require('randomstring');
 var fs = require('fs');
 
-// If set to true, do not use a random value
-var USE_FULL_FACTOR = true;
+var MAX_FACTOR = 4;
+var MAX_DEPTH = 40;
 
-function createObject(factor, depth) {
-  factor = (factor > 0) ? factor : 0;
+function makeBetweenMaxAndMin(value) {
+  return (value < 1) ? 1 : (value > MAX_FACTOR) ? MAX_FACTOR : value
+}
+function makeBetweenMaxAndMinDepth(value) {
+  return (value < 1) ? 1 : (value > MAX_DEPTH) ? MAX_DEPTH : value
+}
+
+function createObject(factor, depth, randomProperties) {
+  console.log('Creating at Depth ', depth);
+  if (!depth) {
+    return randomstring.generate()
+  }
+  factor = makeBetweenMaxAndMin(factor);
   var newObject = {};
-  var min = Math.pow(10, factor);
-  var max = Math.pow(10, factor + 1);
-  var amount = (USE_FULL_FACTOR) ? max : Math.random() * (max - min) + min;
+  var min = 1; //Math.pow(10, factor - 1);
+  var max = Math.pow(10, factor);
+  var amount = (randomProperties) ? Math.random() * (max - min) + min : max;
   for(var x = 0; x < amount; x++) {
-    newObject[randomstring.generate()] = (depth) ? createObject(factor - 1, depth - 1) : randomstring.generate()
+    newObject[randomstring.generate()] = createObject(factor - 1, depth - 1)
   }
   return newObject
 }
 
 function extractTimes(optsToAddTo, times) {
-  ['stringified', 'jsoned'].forEach(function (time) {
-    ['action', 'console', 'log'].forEach(function (test) {
-      optsToAddTo[time + '_' + test] = times[time][test];
-    })
+  ['stringified', 'console', 'log', 'parse'].forEach(function (test) {
+    optsToAddTo[test] = times[test];
   });
   return optsToAddTo;
 }
 
-function timeIt(doThis, attempts) {
-  var totalActionTime = 0;
+function timeIt(whichStringify, objectToTest, attempts) {
+  fs.writeFileSync('./testPrinting.log', '');
+  var totalStringifiedTime = 0;
   var totalConsoleTime = 0;
   var totalLogTime = 0;
+  var totalParseTime = 0;
   for(var x = 0; x < attempts; x++) {
-    var times = printAndTimeIt(function () {
-      return doThis();
-    });
+    console.log('attempt ', x);
+    var times = printAndTimeIt(whichStringify, objectToTest);
     // We want to add up the time differences
-    totalActionTime += times.actionTime;
+    totalStringifiedTime += times.stringifiedTime;
     totalConsoleTime += times.consoleTime;
     totalLogTime += times.logTime;
+    totalParseTime += times.parseTime;
   }
   return {
-    action: totalActionTime / attempts,
+    stringified: totalStringifiedTime / attempts,
     console: totalConsoleTime / attempts,
-    log: totalLogTime / attempts
+    log: totalLogTime / attempts,
+    parse: totalParseTime / attempts
   }
 }
 
-function writeOutFactorAndDepth(factor, depth) {
-  return 'objects with between ' +  Math.pow(10, factor) +' and ' + Math.pow(10, factor + 1) +
+function writeOutFactorAndDepth(factor, depth, randomProperties) {
+  var between = (randomProperties) ? 'between ' +  Math.pow(10, factor - 1) + ' and ' : '';
+  return 'objects with ' + between + Math.pow(10, factor) +
     ' properties, and a depth of ' + depth;
 }
 
-function printAndTimeIt(doThis) {
+function printAndTimeIt(whichStringify, objectToTest) {
   var start = new Date();
-  var value = doThis();
-  var actionTime = new Date();
-  console.log(value);
+  var stringified = JSON[whichStringify](objectToTest);
+  var stringifyTime = new Date();
+  console.log(stringified);
   var consoleTime = new Date();
-  fs.appendFileSync('./testPrinting.log', value);
+  fs.appendFileSync('./testPrinting.log', stringified);
   var logTime = new Date();
+  var newObject = JSON.parse(stringified);
+  var parseTime = new Date();
   return {
     start: start,
-    actionTime: actionTime - start,
-    consoleTime: consoleTime - actionTime,
+    stringifiedTime: stringifyTime - start,
+    consoleTime: consoleTime - stringifyTime,
     logTime: logTime - consoleTime,
-    finish: logTime
+    parseTime: parseTime - logTime,
+    finish: parseTime
   };
 }
 
-function runTests(objectToTest, attempts) {
-  // First stringify to see if we even can do it
-  var whichStringify = 'stringify';
-  try {
-    var stringified = JSON.stringify(objectToTest);
-  } catch (err) {
-    whichStringify = 'stringifyOnce';
-    stringified = JSON.stringifyOnce(objectToTest);
-  }
+function runTests(objectToTest, attempts, useStringifyOnce) {
+  // Since req can't stringify because it's circular
+  var whichStringify = (useStringifyOnce) ? 'stringifyOnce': 'stringify';
 
-  var stringifiedTime = timeIt(function () {
-    return JSON[whichStringify](objectToTest);
-  }, attempts);
-
-  var jsonedTime = timeIt(function () {
-    return JSON.parse(stringified);
-  }, attempts);
-
-  return {
-    stringified: stringifiedTime,
-    jsoned: jsonedTime
-  }
+  return timeIt(whichStringify, objectToTest, attempts);
 
 }
 
 module.exports = {
   createObject: createObject,
   runTests: runTests,
-  writeIt: writeOutFactorAndDepth,
-  extractTimes: extractTimes
+  writeOutFactorAndDepth: writeOutFactorAndDepth,
+  extractTimes: extractTimes,
+  makeBetweenMaxAndMin: makeBetweenMaxAndMin,
+  makeBetweenMaxAndMinDepth: makeBetweenMaxAndMinDepth
 };
