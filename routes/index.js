e724682f8028aff140b8515f8b464b159acc030d
Nathan219 <Nathan219@gmail.com>
2016-05-05 17:46:32 -0700

    adding COMMENTS and shit

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,13 @@ var express = require('express');
 var utils = require('../util.js');
 var router = express.Router();
 
-/* GET home page. */
-//router.get('/hard', function(req, res) {
-//  var factor = 1;
-//  var depth = 0;
-//  var attempts = 2;
-//  var goalTime = 1000;
-//  var times = {};
-//  do {
-//    factor++;
-//    depth = 0;
-//    for(; depth < 4; depth) {
-//      var newObject = utils.createObject(factor, depth);
-//      times = utils.runTests(newObject, attempts);
-//    }
-//  } while (times.jsonedTime < goalTime && times.jsonedTime < goalTime);
-//  var message = utils.writeOutFactorAndDepth(factor, depth);
-//  res.render('index', utils.extractTimes({
-//    title: 'Express',
-//    message: message
-//  }, times));
-//});
-
 router.get('/diy', function(req, res) {
   var factor = parseInt(req.query.factor) || 1;
   var depth =  parseInt(req.query.depth) || 2;
   var randomProperties = !!req.query.random;
 
-  depth = utils.makeBetweenMaxAndMinDepth(depth);
-  factor = utils.makeBetweenMaxAndMin(factor);
+  depth = utils.minMaxDepth(depth);
+  factor = utils.minMaxFactor(factor);
   if (factor > 2) {
     randomProperties = true;
   }
--- util.js
+++ util.js
@@ -5,19 +5,27 @@ var fs = require('fs');
 var MAX_FACTOR = 4;
 var MAX_DEPTH = 40;
 
-function makeBetweenMaxAndMin(value) {
+function minMaxFactor(value) {
   return (value < 1) ? 1 : (value > MAX_FACTOR) ? MAX_FACTOR : value
 }
-function makeBetweenMaxAndMinDepth(value) {
+function minMaxDepth(value) {
   return (value < 1) ? 1 : (value > MAX_DEPTH) ? MAX_DEPTH : value
 }
 
+/**
+ * Creates an object based on the given inputs
+ * @param factor           {Number}  Factor of 10 which should the upper bound of properties created
+ * @param depth            {Number}  Amount of layers of sub-objects created
+ * @param randomProperties {Boolean} True if the amount of properties was between 10 * factor-1 and
+ *                                   10 * factor properties
+ * @returns {{Object}} Hopefully what you asked for
+ */
 function createObject(factor, depth, randomProperties) {
   console.log('Creating at Depth ', depth);
   if (!depth) {
     return randomstring.generate()
   }
-  factor = makeBetweenMaxAndMin(factor);
+  factor = minMaxFactor(factor);
   var newObject = {};
   var min = Math.pow(10, factor - 1);
   var max = Math.pow(10, factor);
@@ -28,6 +36,16 @@ function createObject(factor, depth, randomProperties) {
   return newObject
 }
 
+/**
+ * Just extracts the times out of TIMES and adds them to OPTSTOADD
+ * @param optsToAddTo {Object} Object to add properties to
+ * @param times             {Object} Times object from RunTests
+ * @param times.stringified {Number} Time taken to stringify the object
+ * @param times.console     {Number} Time taken to print the stringified to console
+ * @param times.log         {Number} Time taken to log the stringified to a file
+ * @param times.parse       {Number} Time taken to JSON parse the stringified
+ * @returns {Object} optsToAddTo with the times added
+ */
 function extractTimes(optsToAddTo, times) {
   ['stringified', 'console', 'log', 'parse'].forEach(function (test) {
     optsToAddTo[test] = times[test];
@@ -35,7 +53,15 @@ function extractTimes(optsToAddTo, times) {
   return optsToAddTo;
 }
 
-function timeIt(whichStringify, objectToTest, attempts) {
+/**
+ * Takes an object to test, and runs the test for ATTEMPTS amount of times.
+ * @param objectToTest     {Object}  This is the object to stringify
+ * @param attempts         {Number}  Amount of times to run the test to increase the precision
+ * @param useStringifyOnce {Boolean} True if we should use stringifyOnce instead of stringify.
+ *                                   Usually for circular objects
+ * @returns {{stringified: Number, console: Number, log: Number, parse: Number}} Timing results
+ */
+function runTests(objectToTest, attempts, useStringifyOnce) {
   fs.writeFileSync('./testPrinting.log', '');
   var totalStringifiedTime = 0;
   var totalConsoleTime = 0;
@@ -43,7 +69,7 @@ function timeIt(whichStringify, objectToTest, attempts) {
   var totalParseTime = 0;
   for(var x = 0; x < attempts; x++) {
     console.log('attempt ', x);
-    var times = printAndTimeIt(whichStringify, objectToTest);
+    var times = printAndTimeIt(objectToTest, useStringifyOnce);
     // We want to add up the time differences
     totalStringifiedTime += times.stringifiedTime;
     totalConsoleTime += times.consoleTime;
@@ -57,14 +83,31 @@ function timeIt(whichStringify, objectToTest, attempts) {
     parse: totalParseTime / attempts
   }
 }
-
+/**
+ * Writes out the selections for Factor and Depth in a human-readable way
+ * @param factor           {Number}  Factor of 10 which was the upper bound of properties created
+ * @param depth            {Number}  Amount of layers of sub-objects created
+ * @param randomProperties {Boolean} True if the amount of properties was between
+ *                                   10 * factor-1 and 10 * factor properties
+ * @returns {String} Human-readable interpretation of all the properties
+ */
 function writeOutFactorAndDepth(factor, depth, randomProperties) {
   var between = (randomProperties) ? 'between ' +  Math.pow(10, factor - 1) + ' and ' : '';
   return 'objects with ' + between + Math.pow(10, factor) +
     ' properties, and a depth of ' + depth;
 }
 
-function printAndTimeIt(whichStringify, objectToTest) {
+/**
+ * Given an object, this function times how long it takes to JSON.stringify it, log it to console,
+ * log it to a file, and even re-parsing that value back to an object.
+ * @param objectToTest     {Object}  This is the object to stringify
+ * @param useStringifyOnce {Boolean} True if we should use stringifyOnce instead of stringify.
+ *                                   Usually for circular objects
+ * @returns {{start: Date, stringifiedTime: number, consoleTime: number, logTime: number, parseTime: number, finish: Date}}
+ */
+function printAndTimeIt(objectToTest, useStringifyOnce) {
+  // In case the object is circular, we may need to use stringifyOnce
+  var whichStringify = (useStringifyOnce) ? 'stringifyOnce': 'stringify';
   var start = new Date();
   var stringified = JSON[whichStringify](objectToTest);
   var stringifyTime = new Date();
@@ -84,19 +127,11 @@ function printAndTimeIt(whichStringify, objectToTest) {
   };
 }
 
-function runTests(objectToTest, attempts, useStringifyOnce) {
-  // Since req can't stringify because it's circular
-  var whichStringify = (useStringifyOnce) ? 'stringifyOnce': 'stringify';
-
-  return timeIt(whichStringify, objectToTest, attempts);
-
-}
-
 module.exports = {
   createObject: createObject,
   runTests: runTests,
   writeOutFactorAndDepth: writeOutFactorAndDepth,
   extractTimes: extractTimes,
-  makeBetweenMaxAndMin: makeBetweenMaxAndMin,
-  makeBetweenMaxAndMinDepth: makeBetweenMaxAndMinDepth
+  minMaxFactor: minMaxFactor,
+  minMaxDepth: minMaxDepth
 };
